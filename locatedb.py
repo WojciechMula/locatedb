b118e343ea45b9cec0d08e1b02f3660ea74fabd1
wojtek <wojtek@a13746d7-5020-4d46-8679-245a07f888a7>
2008-12-22 20:16:10 +0000

    added code for join two database files
    git-svn-id: file:///mnt/data/SVNrepo@1746 a13746d7-5020-4d46-8679-245a07f888a7

--- locatedb.py
+++ locatedb.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: iso-8859-2 -*-
-# $Date: 2008-06-08 23:00:14 $, $Revision: 1.9 $
+# $Date: 2008-12-22 20:16:10 $, $Revision: 1.10 $
 #
 # Library/utility for reading and writing locatedb (v2.0) files.
 # Read man locate, man locatedb for details.
@@ -106,7 +106,7 @@ def decompress_aux(file):
 
 		# and create real path
 		current_path = current_path[:count] + path
-		yield current_path
+		yield (count, diff, current_path)
 
 
 def decompress(file):
@@ -117,7 +117,7 @@ def decompress(file):
 	except StopIteration:
 		raise ValueError("Not a locate v.2 file")
 
-	if first_entry != "LOCATE02":
+	if first_entry[2] != "LOCATE02":
 		raise ValueError("Not a locate v.2 file")
 
 	for item in iter:
@@ -128,14 +128,16 @@ usage_text = """\
 Utility to read/write locatedb 2.0 files.
 
 usage:
-	%s decompress infile
-	%s compress path outfile
-	%s compress-list infile outfile
+	__name__ decompress infile
+	__name__ compress path outfile
+	__name__ compress-list infile outfile
+	__name__ join infile1 infile2 outfile
 
 decompres     - print on stdout decompressed contents of infile 
 compress      - create locatedb file for given path
 compress-list - create locatedb from arbitrary list of paths
                 that are readed from infile
+join          - join two locatedb files; no recoding is needed
 
 If infile/outfile is "-" or "--" then file is associated with stdin/stdout.
 """
@@ -145,7 +147,7 @@ if __name__ == "__main__":
 
 	def usage(onerror=False):
 		name = sys.argv[0]
-		sys.stdout.write(usage_text % (name, name, name))
+		sys.stdout.write(usage_text.replace("__name__", name))
 		if onerror:
 			sys.exit(1)
 		else:
@@ -237,8 +239,82 @@ if __name__ == "__main__":
 				die("File", infile, "doesn't exists")
 			infile = open(infile, "rb")
 
-		for path in decompress(infile):
+		for (_, _, path) in decompress(infile):
 			print path
+
+	###
+	elif action == "join":
+		
+		infile1 = get_option(2)
+		infile2 = get_option(3, "-")
+		outfile = get_option(4, "-")
+
+		if not os.path.exists(infile1):
+			die("File", infile1, "doesn't exists")
+		infile1 = open(infile1, "rb")
+
+		if infile2 in ["-", "--"]:
+			infile2 = sys.stdin
+		else:
+			if not os.path.exists(infile2):
+				die("File", infile2, "doesn't exists")
+			infile2 = open(infile2, "rb")
+
+		if outfile in ["-", "--"]:
+			outfile = sys.stdout
+		else:
+			if os.path.exists(outfile):
+				die("File", outfile, "already exists")
+			outfile = open(outfile, "wb")
+
+
+		# get last count - a length of common prefix
+		count = None
+		for (count, _, _) in decompress(infile1):
+			pass
+
+		# 1. copy infile1
+		infile1.seek(0)
+		while True:
+			bytes = infile1.read(4096)
+			if not bytes:
+				break
+			else:
+				outfile.write(bytes)
+
+		# 2. put difference value -count
+		if count is not None:
+			diff  = -count
+			bytes = ""
+			# encode diff
+			if abs(diff) <= 127:
+				# diff in [-127..127]
+				bytes += chr(diff & 0xff)
+			else:
+				# diff larger
+				bytes += chr(0x80)
+
+				hilo   = diff & 0xffff
+				bytes += chr((hilo >> 8) & 0xff)
+				bytes += chr(hilo & 0xff)
+		else:
+			bytes = "\x00"
+
+		outfile.write(bytes)
+
+		# 3. copy infile2, but without first entry (LOCATE02) and without diff=0 of second entry
+		bytes = infile2.read(11)
+		if bytes != "\x00LOCATE02\x00\x00":
+			die(infile2.name, "is not a locate v 2.0 file")
+
+		while True:
+			bytes = infile2.read(4096)
+			if not bytes:
+				break
+			else:
+				outfile.write(bytes)
+
+		outfile.close()
 
 	else:
 		usage(True)
